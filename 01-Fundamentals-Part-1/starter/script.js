7831df22d3046ecdf9623ff5c3e72487078ddaa9
Alex.and.ra <alexandra.velikova.a34@learn.telerikacademy.com>
2023-03-01 09:54:32 +0200

    if-else

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -3,7 +3,7 @@
 // console.log(40 + 8 + 23 - 10);
 // console.log("Jonas");
 // console.log(23);
-
+/*
 let firstName = "Jonas";
 console.log(firstName);
 const job = 'teacher';
@@ -12,3 +12,23 @@ const year = 2022;
 
 const jonas = `Hi, I'm ${firstName}, a ${year - birthYear} years old teacher!`;
 console.log(jonas);
+
+
+const age = 15;
+const isOldEnough = age >= 18;
+if (isOldEnough) {
+    console.log('Sarah can start driving lessons 🚕')
+} else {
+    const yearsLeft = 18 - age;
+    console.log(`Sarah is too young. Wait another ${yearsLeft} years :)`);
+}
+*/
+
+const birthYear = 1991;
+let century;
+if (birthYear <= 2000) {
+    century = 20;
+} else {
+    century = 21;
+}
+console.log(century);
