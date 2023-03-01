6be34ad7e971b21dba84fddf3441417c07009626
Alex.and.ra <alexandra.velikova.a34@learn.telerikacademy.com>
2023-03-01 18:39:28 +0200

    Functions plus error handling

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -6,7 +6,7 @@ if (passTest) hasDriversLicense = true;
 if (hasDriversLicense) console.log("Drive")
 
 const interface = 'Audio';
-*/
+
 
 function describeCountry(country, population, capitalCity) {
     return `${country} has ${population} people and its capital city is ${capitalCity}
@@ -18,3 +18,50 @@ const describeBulgaria = describeCountry('Bulgaria', 6800000, 'Sofia');
 const describeRussia = describeCountry('Russia', 143000000, 'Moscow')
 console.log(describeFinland, describeBulgaria, describeRussia);
 
+const birthYear = 1991
+const calcAge = function (birthYear) {
+    return new Date().getFullYear() - birthYear;
+}
+
+const calcAge2 = birthYear => new Date().getFullYear() - birthYear;
+
+const yearsUntilRetirement = birthYear => 65 - (new Date().getFullYear() - birthYear);
+
+console.log(yearsUntilRetirement(birthYear));
+
+
+function cutFruitPieces(fruit) {
+    return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges) {
+    const applePieces = cutFruitPieces(apples);
+    const orangePieces = cutFruitPieces(oranges);
+    const juice = `Juice with ${applePieces} pieces of apple and ${orangePieces} pieces of orange`
+    return juice;
+}
+*/
+
+const calcAge = function (birthYear) {
+    return new Date().getFullYear() - birthYear;
+}
+
+const calcYearsToRetirement = function (birthYear, firstName) {
+    try {
+        if (typeof (firstName) !== String || typeof (birthYear) !== Number) {
+            throw 'Input format is not correct';
+        } else {
+            const yearsToRetirement = 65 - calcAge(birthYear);
+            if (yearsToRetirement >= 0) {
+                console.log(`${firstName} retires in ${yearsToRetirement} years`);
+                return yearsToRetirement;
+            } else {
+                console.log(`${firstName} has already retired`);
+            }
+        }
+    } catch (err) {
+        prompt("Error: " + err + ".")
+    }
+}
+
+calcYearsToRetirement('Sasha', 1988);
