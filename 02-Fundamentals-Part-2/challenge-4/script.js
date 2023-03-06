ccc486d27a5b612531519e991351d18a0a6fd071
sasha <avelikova@nextforhr.com>
2023-03-06 13:54:48 +0200

    Challenge 4

---
+++ 02-Fundamentals-Part-2/challenge-4/script.js
@@ -0,0 +1,37 @@
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
+const calcTip = function (bill) {
+    let tipPercentage;
+    if (bill > 50 && bill < 300) {
+        tipPercentage = 15;
+    } else tipPercentage = 20;
+    return bill * tipPercentage / 100;
+};
+
+// variant 1
+for (let i = 0; i < bills.length; i++) {
+    tips.push(calcTip(bills[i]));
+    totals.push(bills[i] + tips[i]);
+}
+console.log(tips, totals);
+
+// Variant 2 - works too
+// for (let i = 0; i < bills.length; i++) {
+//     tips[i] = (calcTip(bills[i]));
+//     totals[i] = bills[i] + tips[i];
+// }
+console.log(tips, totals);
+
+function calcAverage(arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    return sum / arr.length;
+}
+
+console.log(calcAverage(totals));
+
+
+
