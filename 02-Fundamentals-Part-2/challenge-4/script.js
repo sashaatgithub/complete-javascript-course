f7c2a40104c32d3e12af0af5b945162770d7714d
sasha <avelikova@nextforhr.com>
2023-03-06 18:05:32 +0200

    challenge - amplitude

--- 02-Fundamentals-Part-2/challenge-4/script.js
+++ 02-Fundamentals-Part-2/challenge-4/script.js
@@ -1,18 +1,19 @@
+"use strict";
 const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
 const tips = [];
 const totals = [];
 const calcTip = function (bill) {
-    let tipPercentage;
-    if (bill > 50 && bill < 300) {
-        tipPercentage = 15;
-    } else tipPercentage = 20;
-    return bill * tipPercentage / 100;
+  let tipPercentage;
+  if (bill > 50 && bill < 300) {
+    tipPercentage = 15;
+  } else tipPercentage = 20;
+  return (bill * tipPercentage) / 100;
 };
 
 // variant 1
 for (let i = 0; i < bills.length; i++) {
-    tips.push(calcTip(bills[i]));
-    totals.push(bills[i] + tips[i]);
+  tips.push(calcTip(bills[i]));
+  totals.push(bills[i] + tips[i]);
 }
 console.log(tips, totals);
 
@@ -24,14 +25,12 @@ console.log(tips, totals);
 console.log(tips, totals);
 
 function calcAverage(arr) {
-    let sum = 0;
-    for (let i = 0; i < arr.length; i++) {
-        sum += arr[i];
-    }
-    return sum / arr.length;
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum / arr.length;
 }
 
 console.log(calcAverage(totals));
-
-
-
+console.log(calcAverage(tips));
--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,63 @@
 // Remember, we're gonna use strict mode in all scripts now!
-'use strict';
+"use strict";
+const inputArray = [3, -2, -6, -1, "error", 9, 13, 17, 15, 14, 9, 5];
+let min;
+let max;
+// function calculateMin(arr) {
+//   let min = arr[0];
+//   for (let i = 1; i < arr.length; i++) {
+//     if (typeof arr[i] === "number" && min > arr[i]) {
+//       min = arr[i];
+//     }
+//   }
+//   return min;
+// }
 
+// function calculateMax(arr) {
+//   let max = arr[0];
+//   for (let i = 1; i < arr.length; i++) {
+//     if (typeof arr[i] === "number" && max < arr[i]) {
+//       max = arr[i];
+//     }
+//   }
+//   return max;
+// }
+
+// function calculateAmplitude(arr) {
+//   return calculateMax(arr) - calculateMin(arr);
+// }
+
+// console.log(calculateAmplitude(inputArray));
+
+// more efficient option 2 which also takes into account the case where first element is error:
+
+function findFirstNumberIndex(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let maybeNumber = arr[i];
+    if (typeof maybeNumber === "number") {
+      return i;
+    }
+  }
+  return "No number found";
+}
+
+function calculateAmplitude(arr) {
+  let firstNumberIndex = findFirstNumberIndex(arr);
+  let firstNumber = arr[firstNumberIndex];
+  let min = firstNumber;
+  let max = firstNumber;
+  for (let i = firstNumberIndex; i < arr.length; i++) {
+    let curr = arr[i];
+    if (typeof curr !== "number") continue;
+    if (curr > max) {
+      max = curr;
+    } else if (curr < min) {
+      min = curr;
+    }
+  }
+  if (max && min) {
+    return max - min;
+  } else return 0;
+}
+
+console.log(calculateAmplitude(inputArray));
