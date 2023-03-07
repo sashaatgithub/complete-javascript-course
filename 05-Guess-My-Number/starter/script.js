935cd6c37d037f404edc74f7c157ed88c2d66110
sasha <avelikova@nextforhr.com>
2023-03-07 19:08:09 +0200

    Added number guessing except number hiding

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,45 @@
 'use strict';
+
+function updateScore() {
+  document.querySelector('.score').textContent =
+    Number(document.querySelector('.score').textContent) - 1;
+}
+
+function updateHighscore() {
+  let score = document.querySelector('.score').textContent;
+  let highscore = document.querySelector('.highscore').textContent;
+  if (score > highscore) {
+    document.querySelector('.highscore').textContent = score;
+  }
+}
+
+function resetHighscore() {
+  document.querySelector('.highscore').textContent = 0;
+}
+
+function checkNumber() {
+  const input = document.querySelector('.guess').value;
+  const secretNumber = document.querySelector('.number').textContent;
+  let output = document.querySelector('.message');
+  if (Number(input) === Number(secretNumber)) {
+    output.textContent = 'Correct Number! ✨';
+    updateHighscore();
+  } else {
+    updateScore();
+    if (Number(input) < Number(secretNumber)) {
+      output.textContent = 'Too low.😢 Try again!';
+    } else {
+      output.textContent = 'Too high.😢 Try again!';
+    }
+  }
+}
+
+function again() {
+  document.querySelector('.guess').value = '';
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.score').textContent = 20;
+}
+
+document.querySelector('.check').addEventListener('click', checkNumber);
+document.querySelector('.again').addEventListener('click', again);
+document.querySelector('.reset').addEventListener('click', resetHighscore);
