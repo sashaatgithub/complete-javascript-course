f9a80b8e091ef7edae1767a97df27335bf863b7e
Alex.and.ra <alexandra.velikova.a34@learn.telerikacademy.com>
2023-03-08 09:09:51 +0200

    Not working variant

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,45 +1,54 @@
-'use strict';
-
-function updateScore() {
-  document.querySelector('.score').textContent =
-    Number(document.querySelector('.score').textContent) - 1;
-}
-
-function updateHighscore() {
-  let score = document.querySelector('.score').textContent;
-  let highscore = document.querySelector('.highscore').textContent;
-  if (score > highscore) {
-    document.querySelector('.highscore').textContent = score;
-  }
-}
-
-function resetHighscore() {
-  document.querySelector('.highscore').textContent = 0;
-}
-
-function checkNumber() {
-  const input = document.querySelector('.guess').value;
-  const secretNumber = document.querySelector('.number').textContent;
-  let output = document.querySelector('.message');
-  if (Number(input) === Number(secretNumber)) {
-    output.textContent = 'Correct Number! ✨';
-    updateHighscore();
-  } else {
-    updateScore();
-    if (Number(input) < Number(secretNumber)) {
-      output.textContent = 'Too low.😢 Try again!';
-    } else {
-      output.textContent = 'Too high.😢 Try again!';
-    }
-  }
-}
-
-function again() {
-  document.querySelector('.guess').value = '';
-  document.querySelector('.message').textContent = 'Start guessing...';
-  document.querySelector('.score').textContent = 20;
-}
-
+let number = generateNumber1To20();
+document.querySelector('.number').textContent = number;
 document.querySelector('.check').addEventListener('click', checkNumber);
 document.querySelector('.again').addEventListener('click', again);
 document.querySelector('.reset').addEventListener('click', resetHighscore);
+
+let generateNumber1To20 = function () {
+    return Math.trunc(Math.random() * 20) + 1;
+}
+function checkNumber() {
+    const input = document.querySelector('.guess').value;
+    const secretNumber = document.querySelector('.number').textContent;
+    let output = document.querySelector('.message');
+    if (!input) {
+        output.textContent = "You need to enter a number!"
+    } else if (Number(input) === Number(secretNumber)) {
+        output.textContent = 'Correct Number! ✨';
+        updateHighscore();
+    } else {
+        updateScore();
+        if (Number(input) < Number(secretNumber)) {
+            output.textContent = 'Too low.😢 Try again!';
+        } else {
+            output.textContent = 'Too high.😢 Try again!';
+        }
+    }
+}
+
+function updateScore() {
+    document.querySelector('.score').textContent =
+        Number(document.querySelector('.score').textContent) - 1;
+}
+
+function updateHighscore() {
+    let score = document.querySelector('.score').textContent;
+    let highscore = document.querySelector('.highscore').textContent;
+    if (score > highscore) {
+        document.querySelector('.highscore').textContent = score;
+    }
+}
+
+function resetHighscore() {
+    document.querySelector('.highscore').textContent = 0;
+}
+
+function again() {
+    number = generateNumber1To20();
+    document.querySelector('.number').textContent = number;
+    document.querySelector('.guess').value = '';
+    document.querySelector('.message').textContent = 'Start guessing...';
+    document.querySelector('.score').textContent = 20;
+}
+
+
