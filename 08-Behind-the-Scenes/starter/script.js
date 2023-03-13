78945a47c0aa30d2eaa2c9ca1aad19af4f0c2c96
sasha <avelikova@nextforhr.com>
2023-03-13 18:53:56 +0200

    Destructuring mainly

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-function calcAge(birthYear) {
+/*function calcAge(birthYear) {
   const age = 2037 - birthYear;
 
   function printAge() {
@@ -14,3 +14,85 @@ function calcAge(birthYear) {
 const firstName = 'Jonas';
 calcAge(1991);
 console.log(calcAge(1991));
+*/
+// console.log(this);
+
+// const calcAge = function (birthYear) {
+//   console.log(2037 - birthYear);
+//   console.log(this);
+// };
+
+// const calcAgeArrow = birthYear => {
+//   console.log(this);
+//   console.log(2037 - birthYear);
+// };
+
+// calcAgeArrow(1991);
+
+// const sasha = {
+//   birthYear: 1988,
+//   calcAge: function () {
+//     this.age = 2022 - this.birthYear;
+//     return this.calcAge;
+//   },
+// };
+// console.log(sasha.calcAge());
+// console.log(sasha.calcAge);
+
+// const mathilda = {
+//   birthYear: 2017,
+// };
+
+// mathilda.calcAge = sasha.calcAge;
+// console.log(mathilda.calcAge());
+
+// const addExpr = function () {
+//   console.log(arguments);
+//   let sum = 0;
+//   for (let i = 0; i < arguments.length; i++) {
+//     sum = sum + arguments[i];
+//   }
+//   return sum;
+// };
+// console.log(addExpr(7, 8, 5));
+
+// let age = 30;
+// let oldAge = age;
+// age = 31;
+// console.log(age);
+// console.log(oldAge);
+
+// const me = {
+//   name: 'Jonas',
+//   age: 30,
+// };
+
+// const friend = me;
+// friend.age = 27;
+// console.log(me, friend);
+
+let lastName = 'Williams';
+let oldLastName = lastName;
+lastName = 'Davis';
+console.log(lastName);
+console.log(oldLastName);
+
+const jessica = {
+  firstName: 'Jessica',
+  lastName: 'Williams',
+  age: 27,
+};
+
+const marriedJessica = {
+  firstName: jessica.firstName,
+  lastName: jessica.lastName,
+  age: jessica.age,
+};
+
+marriedJessica.lastName = 'Davis';
+
+console.log(jessica, marriedJessica);
+
+const jessicaCopy = Object.assign({}, jessica);
+jessicaCopy.lastName = 'Kingston';
+console.log(jessicaCopy);
--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -12,6 +12,9 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
   openingHours: {
     thu: {
       open: 12,
@@ -26,4 +29,70 @@ const restaurant = {
       close: 24,
     },
   },
+
+  orderDelivery: function ({
+    time = new Date().toTimeString(),
+    mainIndex,
+    address,
+  }) {
+    console.log(
+      `Order received. ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
+  },
 };
+
+restaurant.orderDelivery({
+  time: '22:30',
+  address: 'Via del Sol, 21',
+  mainIndex: 2,
+  starterIndex: 2,
+});
+
+const myOrder = {
+  mainIndex: 0,
+  address: 'Bulgaria blvd, 108',
+  comment: 'ASAP',
+};
+
+restaurant.orderDelivery(myOrder);
+
+// const arr1 = [17, 'Jonas', ['heyho', 'yuppee']];
+// const [x, y, z] = arr1;
+// console.log(x, y, z, arr1);
+// let [origin, , foodType] = restaurant.categories;
+// console.log(origin);
+// console.log(foodType);
+
+// [foodType, origin] = [origin, foodType];
+// console.log(foodType);
+
+// const [starterCourse, mainCourse] = restaurant.order(3, 1);
+// console.log(starterCourse);
+
+// const nested = [2, 4, [5, 6]];
+// const [i, , [ji, jj]] = nested;
+// console.log(ji);
+// console.log(jj);
+
+// const [p = 0, q = 0, r = 0] = [8, 9];
+// console.log(r);
+
+// const { name: restaurantName, openingHours: hours } = restaurant;
+// console.log(restaurantName, hours);
+
+// const { menu = [], starterMenu: starters = [] } = restaurant;
+
+// const {
+//   openingHours: {
+//     fri: { open: fridayOpening },
+//   },
+// } = restaurant;
+// console.log(fridayOpening);
+
+// let a = 111;
+// let b = 999;
+// const obj = { a: 23, b: 7, c: 14 };
+// ({ a, b } = obj);
+
+const [starter, mainCourse] = restaurant.order(2, 0);
+console.log(starter, mainCourse);
