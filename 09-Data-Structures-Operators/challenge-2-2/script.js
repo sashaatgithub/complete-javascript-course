1b46b4284f0884ab49d71dc574461bb2f6699e33
Alex.and.ra <alexandra.velikova.a34@learn.telerikacademy.com>
2023-05-23 09:10:36 +0300

    Challenge 2 of data structures finalized

--- 09-Data-Structures-Operators/challenge-2-2/script.js
+++ 09-Data-Structures-Operators/challenge-2-2/script.js
@@ -42,3 +42,32 @@ const game = {
 
 for (const [numberMinus1, name] of game.scored.entries()) { console.log(`Goal ${numberMinus1 + 1}: ${name}`) }
 
+let sum = 0;
+let numberOfOdds = 0
+for (const odd of Object.values(game.odds)) {
+  sum = odd + sum;
+  numberOfOdds = numberOfOdds + 1;
+} console.log(`The average odds are ${sum / numberOfOdds}`)
+
+let whoseOdds = [];
+let odds = [];
+for (const [whoseOdd, odd] of Object.entries(game.odds)) {
+  whoseOdds = [...whoseOdds, whoseOdd];
+  odds = [...odds, odd];
+  console.log(whoseOdd);
+  console.log(odd);
+}
+
+const teamNames = {}
+for (const whoseOdd of whoseOdds) {
+  teamNames[whoseOdd] ??= game[whoseOdd] ?? 'draw';
+}
+console.log(teamNames);
+
+whoseOdds.forEach((whoseOdd, index) => {
+  let teamName = game[whoseOdd] ?? 'draw';
+  let odd = odds[index];
+  let victoryOf = `victory of ${teamName}`;
+  let scenarioName = `${teamName === "draw" ? "draw" : `${victoryOf}`}`
+  console.log(`Odd of ${scenarioName} is ${odd}`);
+})
