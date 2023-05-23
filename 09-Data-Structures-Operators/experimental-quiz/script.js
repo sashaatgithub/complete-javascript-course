5b7f1b6f951ae91012922649853923979501905e
Alex.and.ra <alexandra.velikova.a34@learn.telerikacademy.com>
2023-05-23 17:08:56 +0300

    Almost all experimental quiz

--- 09-Data-Structures-Operators/experimental-quiz/script.js
+++ 09-Data-Structures-Operators/experimental-quiz/script.js
@@ -1,29 +1,44 @@
 'use strict';
 
 const question = new Map([
-  ['question', 'What language are we learning?'],
+  ['question', 'What language are we learning? You have only one guess'],
   [1, 'C'],
   [2, 'Java'],
   [3, 'JavaScript'],
   ['correct', 3],
   [true, 'Correct✔'],
-  [false, 'Try again😕'],
+  [false, 'Incorrect😕'],
   ['questionElement', document.querySelector('h1')],
   ['answerElement', document.querySelector('#inputAnswer')],
-  ['hintText', document.querySelector('.hint')]
+  ['hintText', document.querySelector('.hint-text')],
+  ['btnHint', document.querySelector('.btn-hint')],
+  ['msgText', document.querySelector('.msg-text')]
 ]);
 console.log(question);
+let hintText = '';
+for (const [key, value] of question) {
+  if (typeof key === 'number') {
+    hintText = hintText + `Answer ${key}: ${value}`;
+    if (key !== 3) {
+      // TODO: Understand why different answers don't appear on different lines.
+      hintText = hintText + '\n';
+    }
+  }
+}
+(question.get('hintText')).textContent = hintText;
 
 (question.get('questionElement')).textContent = question.get('question');
+(question.get('btnHint')).addEventListener('click', toggleHint)
 
-
-for (const [key, value] of question) {
-  if (typeof key === 'number')
-    console.log(`Answer ${key}: ${value}`);
+function toggleHint() {
+  (question.get('hintText')).classList.toggle('hidden');
 }
+
+(question.get('msgText')).textContent = (question.get('answerElement')).textContent === question.get(question.get('correct'));
+(question.get('answerElement')).textContent === question.get(question.get('correct'));
+
 // const answer = Number(prompt('Enter your answer here.'))
 
 // console.log(question.get(answer === question.get('correct')));
 
 console.log(question);
-question.get('hintText').classList.add('hidden');
