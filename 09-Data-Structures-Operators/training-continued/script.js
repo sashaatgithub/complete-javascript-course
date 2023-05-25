72a6713c589d7d816653768ffb6b0405638042bf
sasha <avelikova@nextforhr.com>
2023-05-25 16:39:26 +0300

    Implemented simpler version of the quiz

---
+++ 09-Data-Structures-Operators/training-continued/script.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const question = new Map([
+  ['question', 'What language are we learning? You have only one guess'],
+  [1, 'C '],
+  [2, 'Java '],
+  [3, 'JavaScript '],
+  ['correct', 3],
+  [true, 'Correct✔'],
+  [false, 'Incorrect😕'],
+  ['questionElement', document.querySelector('h1')],
+  ['hintText', document.querySelector('.hint-text')],
+  ['msgText', document.querySelector('.msg-text')],
+]);
+console.log(question);
+
+// let hintText = 'abc';
+let hintText = '';
+for (const [key, value] of question) {
+  if (typeof key === 'number') {
+    hintText = hintText + `Answer ${key}: ${value}`;
+  }
+}
+
+const answer = Number(
+  prompt('Enter the number of the correct answer here. ' + hintText)
+);
+
+question.get('msgText').textContent = question.get(
+  answer === question.get('correct')
+);
+
+console.log(question.get(answer === question.get('correct')));
