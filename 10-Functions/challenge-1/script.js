581f61ac6947b83a90da5d422f2b41faa106ec83
sasha <avelikova@nextforhr.com>
2023-05-29 12:16:45 +0300

    Finalized answer poll

---
+++ 10-Functions/challenge-1/script.js
@@ -0,0 +1,54 @@
+'use strict';
+const poll = {
+  question: 'What is your favourite programming language?',
+  options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+  // This generates [0, 0, 0, 0]. More in the next section!
+  answers: new Array(4).fill(0),
+};
+
+document.querySelector('.poll').addEventListener('click', registerNewAnswer);
+document.querySelector('.show').addEventListener('click', toggleResults);
+
+function registerNewAnswer() {
+  debugger;
+  let optionsStr = '';
+  let newAnswer = '';
+  const options = poll.options;
+  options.forEach(element => {
+    optionsStr = optionsStr + element + '\n';
+  });
+  let promptText = `${poll.question}\n ${optionsStr}\n (Write valid option number)`;
+  while (true) {
+    newAnswer = Number(prompt(promptText));
+    if (newAnswer >= 0 && newAnswer < options.length) {
+      poll.answers[newAnswer] = poll.answers.at(newAnswer) + 1;
+      break;
+    }
+    if (promptText.startsWith(poll.question)) {
+      promptText =
+        'You have entered an invalid answer. Please try again! \n' + promptText;
+    }
+  }
+  createResultsDisplay();
+  toggleResults();
+}
+
+function createResultsDisplay() {
+  let answersElement = document.querySelector('.answers');
+  const answers = poll.answers;
+  console.log(answers);
+  const options = poll.options;
+  let answersStr = '';
+  for (let i = 0; i < options.length; i++) {
+    answersStr = `${answersStr} ${options[i]} - ${answers[i]} replies`;
+    if (i < options.length - 1) {
+      answersStr = answersStr + '; ';
+    }
+  }
+  answersElement.innerHTML = answersStr;
+}
+
+function toggleResults() {
+  createResultsDisplay();
+  document.querySelector('.answers').classList.toggle('hidden');
+}
