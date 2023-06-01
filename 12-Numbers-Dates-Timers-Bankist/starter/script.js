c302193b0afd1126a484ca52b4c151b25ba8293a
sashaatgithub <avelikova@nextforhr.com>
2023-06-01 08:55:55 +0300

    implemented loan timer

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -207,13 +207,15 @@ btnLoan.addEventListener('click', function (e) {
 
   const amount = +(inputLoanAmount.value);
 
-  if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
-    // Add movement
-    currentAccount.movements.push(amount);
+  setTimeout(function () {
+    if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
+      // Add movement
+      currentAccount.movements.push(amount);
 
-    // Update UI
-    updateUI(currentAccount);
-  }
+      // Update UI
+      updateUI(currentAccount);
+    }
+  }, 4000);
   inputLoanAmount.value = '';
 });
 
