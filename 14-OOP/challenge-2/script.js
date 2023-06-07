766dec098b51d500e73601e07645224fbc382dc5
sashaatgithub <avelikova@nextforhr.com>
2023-06-07 10:20:48 +0300

    OOP challenge-2 start

--- 14-OOP/challenge-2/script.js
+++ 14-OOP/challenge-2/script.js
@@ -1,4 +1,60 @@
 'use strict'
 
-// class CarCl{
-// }
+class CarCl {
+  constructor(make, speedKmh) {
+    this.make = make;
+    this.speedKmh = speedKmh;
+  }
+  // Different setters are used for actuallyt setting and changing names;
+  set make(receivedName) {
+    if (receivedName === ' ' || receivedName === '') { alert('Name cannot be empty') }
+    else {
+      this._make = receivedName;
+    }
+  }
+
+  set changeMake(receivedName) {
+    if (receivedName === ' ' || receivedName === '') { alert('Name cannot be empty') }
+    else {
+      this._make = receivedName;
+    }
+  }
+  get make() { return this._make; }
+
+  accelerate() {
+    const newSpeed = this.speedKmh + 10;
+    this.speedKmh = newSpeed;
+    return this.speedKmh;
+  }
+
+  brake() {
+    this.speedKmh = this.speedKmh - 5;
+    return this.speedKmh;
+  }
+
+  speedMpH() {
+    return this.speedKmh / 1.6;
+  }
+}
+
+const ford = new CarCl('Ford', 120);
+
+
+console.log(ford.speedMpH());
+ford.accelerate();
+console.log(ford.speedMpH());
+ford.brake();
+console.log(ford.speedMpH());
+console.log(ford.make);
+const fusion = new CarCl('F1', 199);
+console.log(fusion);
+console.log(fusion.make);
+
+
+
+
+
+
+
+
+
