f80825086fa4ad713dbb89809612d03a32c72fee
sashaatgithub <avelikova@nextforhr.com>
2023-06-08 17:22:26 +0300

    consuming promises

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -10,3 +10,4 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+const countriesAPILink = 'https://countries-api-836d.onrender.com/countries/'
--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -2,5 +2,61 @@
 
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
-
+const countriesAPILink = 'https://countries-api-836d.onrender.com/countries'
 ///////////////////////////////////////
+
+// function getCountryData(country) {
+//     const request = new XMLHttpRequest();
+//     request.open('GET', `${countriesAPILink}/name/${country}`);
+//     request.send();
+//     request.addEventListener('load', function () {
+//         // 'this' is the request
+//         const [data] = JSON.parse(this.responseText);
+//         console.log(data)
+
+//         const html = `<article class="country">
+// <img class="country__img" src="${data.flag}" />
+// <div class="country__data">
+//   <h3 class="country__name">${data.name}</h3>
+//   <h4 class="country__region">${data.region}</h4>
+//   <p class="country__row"><span>👫</span>${(data.population / 1000000).toFixed(1)}</p>
+//   <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+//   <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+// </div>
+// </article>`;
+
+//         countriesContainer.insertAdjacentHTML('beforeend', html)
+//         countriesContainer.style.opacity = 1;
+//     });
+// }
+// getCountryData('Russia');
+// getCountryData('burundi');
+// getCountryData('Iraq');
+
+function renderCountry(data) {
+    const html = `<article class="country">
+<img class="country__img" src="${data.flag}"/>
+<div class="country__data">
+  <h3 class="country__name">${data.name}</h3>
+  <h4 class="country__region">${data.region}</h4>
+  <p class="country__row"><span>👫</span>${(data.population / 1000000).toFixed(1)}</p>
+  <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+  <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+</div>
+</article>`;
+
+    countriesContainer.insertAdjacentHTML('beforeend', html)
+    countriesContainer.style.opacity = 1;
+}
+
+
+// from fetch, we get a Promise immediately
+function getCountryData(country) {
+    const request = fetch(`${countriesAPILink}/name/${country}`)
+        .then(response => response.json())
+        .then(data => renderCountry(data[0]));
+};
+
+getCountryData('italy');
+
+
