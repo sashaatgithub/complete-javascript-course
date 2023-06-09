dc1c5c996c118313d0a028e31aa55366d600f74a
sashaatgithub <avelikova@nextforhr.com>
2023-06-09 16:41:39 +0300

    adding experiment with promise and timer

--- 16-Asynchronous/challenge-1/script.js
+++ 16-Asynchronous/challenge-1/script.js
@@ -1,22 +1,33 @@
 'use strict';
-
+/*
 const apiBaseURL = 'https://geocode.xyz/';
 const formatParam = 'geoit=json';
-
-
 
 function getCity(latitude, longitude) {
     const response = fetch(`${apiBaseURL}/${latitude},${longitude}?${formatParam}`)
         .then(response => {
-            if (!response.ok) {
-                const message = `Something went wrong ${response.status}`;
-                console.log(message);
-                throw new Error(message);
-            } else return response.json();
+            let message;
+            if (response.ok) {
+                return response.json()
+            }
+            if (response.error) {
+                message = `Something went wrong: ${error.description}`;
+
+            } else {
+                message = `Something went wrong ${response.status}`;
+            } throw new Error(message)
         })
         .then(response => {
             console.log(response);
             console.log(response.city);
+            return response;
         })
+        .catch(err => console.log(err.message))
 }
-getCity(55.74, 37.66);
+getCity(155.74, 237.66);
+*/
+console.log('Test start');
+setTimeout(() => console.log('0 sec timer 🏆'), 0);
+Promise.resolve('Resolved promise 1').then(res => console.log(res));
+console.log('Test end');
+
