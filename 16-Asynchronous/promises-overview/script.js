5d4719b70cef7f26a32b3d4b21de3192eca54eb6
sashaatgithub <avelikova@nextforhr.com>
2023-06-14 16:25:07 +0300

    added own example of async await function

--- 16-Asynchronous/promises-overview/script.js
+++ 16-Asynchronous/promises-overview/script.js
@@ -1,17 +1,38 @@
 "use strict";
+/*
 // the function should produce the resulting value of a future promise
 const lotteryPromise = new Promise(function (resolve, reject) {
-  const lotteryDraw = Math.random();
-  if (lotteryDraw >= 0.5) {
-    resolve(`You won ${Math.round(lotteryDraw * 1000)} BGN`);
-  } else {
-    reject("You lost your money");
-  }
+  console.log("Lottery draw is happening");
+  setTimeout(() => {
+    const lotteryDraw = Math.random();
+    if (lotteryDraw >= 0.5) {
+      resolve(`You won ${Math.round(lotteryDraw * 1000)} BGN`);
+    } else {
+      reject(new Error("You lost your money"));
+    }
+  }, 2000);
 });
 console.log(lotteryPromise);
-lotteryPromise.then((res) => console.log(res)).catch((err) => console.log(err));
+lotteryPromise
+  .then((res) => console.log(res))
+  .catch((err) => console.error(err));
 
-/*
+function timerInSeconds(seconds) {
+  return new Promise(function (resolve) {
+    return setTimeout(() => resolve(), seconds * 1000);
+  });
+}
+
+timerInSeconds(5).then(() => {
+  const lotteryDraw = Math.random();
+  if (lotteryDraw >= 0.5) {
+    return `You won ${Math.round(lotteryDraw * 1000)} BGN`;
+  } else {
+    return new Error("You lost your money");
+  }
+});
+
+
 const countriesNameAPILink =
   "https://countries-api-836d.onrender.com/countries/name";
 
@@ -38,3 +59,16 @@ const getCountryData = function (country) {
 
 getCountryData("france");
 */
+
+const getMaximumTemperature = async function () {
+  const result = await fetch(
+    "https://api.open-meteo.com/v1/forecast?latitude=43.42&longitude=28.16&hourly=temperature_2m"
+  );
+  const expectedTemperature = await result.json();
+  const max = await expectedTemperature.hourly.temperature_2m.reduce(
+    (acc, el) => Math.max(acc, Number(el))
+  );
+  console.log(max);
+};
+
+getMaximumTemperature();
