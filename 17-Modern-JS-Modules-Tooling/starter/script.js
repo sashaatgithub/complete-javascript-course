8323a888b147e6807997ebac3a46617683c576ca
sashaatgithub <avelikova@nextforhr.com>
2023-06-16 13:09:56 +0300

    deleted npm folder

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -8,6 +8,15 @@ const state = {
   user: { loggedIn: true },
 };
 
-const stateClone = Object.assign({}, state);
-console.log(stateClone);
-state.user.loggedIn = false;
+console.log(state);
+
+const stateCloneAssign = Object.assign({}, state);
+console.log(stateCloneAssign);
+
+const stateCloneStructured = structuredClone(state);
+console.log(stateCloneStructured);
+
+// Even the initial console.logs before the change are mutated
+stateCloneStructured.user.loggedIn = false;
+console.log(stateCloneStructured);
+Object.freeze(stateCloneStructured);
